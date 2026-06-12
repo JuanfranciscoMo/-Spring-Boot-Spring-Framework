ff25d2dac11e3935eb1f6721a1c411d57d7ee6c3
Juan Francisco Morales <juanfrancmorales94@gmail.com>
2026-06-12 12:21:20 -0600

    Spring MVC

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,19 +1,67 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
+
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index","/", "/home"})
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
+	
+	@GetMapping({"/index","/", "/home", ""})
 	public ModelAndView index(ModelAndView mv) {
-		mv.addObject("titulo", "Hola Spring Framework con Map!");
+		mv.addObject("titulo", textoIndex);
 		mv.setViewName("index");
 		return mv;
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Juan");
+		usuario.setApellido("Morales");
+		usuario.setEmail("ejemplo@gmail.com");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo",textoPerfil.concat(usuario.getNombre() + " "+ usuario.getApellido()));
+		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		
+		
+		model.addAttribute("titulo",textoListar);
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(
+				new Usuario("Juan","Morales","juan@email.com"),
+				new Usuario("Ramón","Morales","ramon@email.com"),
+				new Usuario("María","Martínez","maria@email.com"),
+				new Usuario("Yazmin","Morales","yazmin@email.com")
+				);
+		return usuarios;
 	}
 
 }
